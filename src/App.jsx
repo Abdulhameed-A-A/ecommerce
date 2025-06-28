3cf607f99e92847fbe113357eeef849051fd0910
Ola.jsx <aaa-dev@A.A.A>
2025-06-28 19:27:37 +0100

    Generate HTML for the the cart items

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { Routes, Route } from 'react-router';
 import { useState, useEffect } from 'react';
 import { HomePage } from './pages/HomePage'
-import { CheckoutPage } from './pages/CheckoutPage';
+import { CheckoutPage } from './pages/Checkout/CheckoutPage';
 import './App.css'
 import { OrdersPage } from './pages/OrdersPage';
 import { TrackingPage } from './pages/TrackingPage';
@@ -11,17 +11,19 @@ function App() {
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
-    axios.get('/api/cart-items')
+    axios.get('/api/cart-items?expand=product')
       .then((response) => {
         setCart(response.data)
       })
   }, [])
 
 
+
+
   return (
-    <Routes> 
-      <Route index element={<HomePage cart={cart}/>} />
-      <Route path="checkout" element={<CheckoutPage cart={cart}/>} />
+    <Routes>
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage />} />
       <Route path="tracking" element={<TrackingPage />} />
       <Route path="*" element={<NotFoundPage />} />
