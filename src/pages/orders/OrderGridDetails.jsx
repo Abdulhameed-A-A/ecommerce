5fc6c6e72e927d65c6977f3dbc9973d7f0f5b272
Ola.jsx <aaa-dev@A.A.A>
2025-06-29 13:47:40 +0100

    Seperate order into smaller component

---
+++ src/pages/orders/OrderGridDetails.jsx
@@ -0,0 +1,42 @@
+import dayjs from 'dayjs';
+import { Fragment } from 'react';
+
+export function OrderGridDetails({ order }) {
+  return (
+    <div className="order-details-grid">
+      {order.products.map((OrderProduct) => {
+        return (
+          <Fragment key={OrderProduct.product.id}>
+            <div className="product-image-container">
+              <img src={OrderProduct.product.image} />
+            </div>
+
+            <div className="product-details">
+              <div className="product-name">
+                {OrderProduct.product.name}
+              </div>
+              <div className="product-delivery-date">
+                Arriving on: {dayjs(OrderProduct.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
+              </div>
+              <div className="product-quantity">
+                Quantity: {OrderProduct.quantity}
+              </div>
+              <button className="buy-again-button button-primary">
+                <img className="buy-again-icon" src="images/icons/buy-again.png" />
+                <span className="buy-again-message">Add to Cart</span>
+              </button>
+            </div>
+
+            <div className="product-actions">
+              <a href="/tracking">
+                <button className="track-package-button button-secondary">
+                  Track package
+                </button>
+              </a>
+            </div>
+          </Fragment>
+        )
+      })}
+    </div>
+  );
+}
